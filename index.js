d4aa23040ac3f9785e243194d40460f2fe54cfa6
Willames Vital <willames.vital@liferay.com>
2023-08-04 23:12:17 -0300

    PERPROJ-001 Add functions salvarComoImagem and copiarTexto

--- index.js
+++ index.js
@@ -41,3 +41,53 @@ function limparCampo() {
 document.getElementById('limparBotao').addEventListener('click', limparCampo);
   
   
+  // Função para salvar o texto como imagem jpg
+  function salvarComoImagem() {
+    const output = document.getElementById('brutoOutput');
+  
+    // Utiliza o html2canvas para renderizar o elemento em um canvas
+    html2canvas(output).then(function (canvas) {
+      // Cria um elemento <a> para fazer o download da imagem
+      const link = document.createElement('a');
+      link.download = 'meu-dizimo.jpg';
+      link.href = canvas.toDataURL('image/jpeg');
+  
+      // Clica no link para iniciar o download
+      link.click();
+    });
+  }
+  
+// Função para copiar o texto para a área de transferência
+function copiarTexto() {
+    const output = document.getElementById('brutoOutput');
+    const texto = output.innerText;
+  
+    // Cria um elemento temporário para copiar o texto
+    const textarea = document.createElement('textarea');
+    textarea.value = texto;
+    document.body.appendChild(textarea);
+  
+    // Seleciona o texto no campo temporário
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); // Para dispositivos móveis
+  
+    // Copia o texto para a área de transferência
+    document.execCommand('copy');
+  
+    // Remove o campo temporário
+    document.body.removeChild(textarea);
+  }
+
+
+  // Adiciona um ouvinte de evento ao botão para atualizar o texto
+document.getElementById('brutoOutput').addEventListener('input', atualizarTexto);
+
+// Adiciona um ouvinte de evento ao botão para limpar o texto
+document.getElementById('limparBotao').addEventListener('click', limparTexto);
+
+// Adiciona um ouvinte de evento ao botão para salvar como imagem
+document.getElementById('salvarBotao').addEventListener('click', salvarComoImagem);
+
+// Adiciona um ouvinte de evento ao botão para copiar o texto
+document.getElementById('copiarBotao').addEventListener('click', copiarTexto);
+
